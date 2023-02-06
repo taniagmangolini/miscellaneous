c97ba151e3cab511560031faada50dd7d20e3968
taniagmangolini <taniagmangolini@gmail.com>
2023-02-05 23:15:41 -0300

    django studies

--- advanced_django/03_01_b/friendsbook/business/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Business
-
-
-admin.site.register(Business)
--- advanced_django/05_04_e/friendsbook/dbtemplates/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import DBTemplate
-
-admin.site.register(DBTemplate)
